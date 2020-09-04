7c255855599cc22e449684b7862cccfda4983e88
khageshwor <khageshwarjoshi03@gmail.com>
2020-09-04 20:44:31 +0545

    Add problem3 solution

---
+++ Subquries_HR_DB/problem3.sql
@@ -0,0 +1,31 @@
+-- 3. Write a query to display the name ( first name and last name ), salary, department id 
+-- for those employees who earn such amount of salary which is the smallest salary of any of the departments
+
+-- Sample table: employees
+
+-- Table
+
+--     +-------------+-------------+-------------+----------+--------------------+------------+------------+----------+----------------+------------+---------------+
+-- | EMPLOYEE_ID | FIRST_NAME  | LAST_NAME   | EMAIL    | PHONE_NUMBER       | HIRE_DATE  | JOB_ID     | SALARY   | COMMISSION_PCT | MANAGER_ID | DEPARTMENT_ID |
+-- +-------------+-------------+-------------+----------+--------------------+------------+------------+----------+----------------+------------+---------------+
+-- |         100 | Steven      | King        | SKING    | 515.123.4567       | 2003-06-17 | AD_PRES    | 24000.00 |           0.00 |          0 |            90 |
+-- |         101 | Neena       | Kochhar     | NKOCHHAR | 515.123.4568       | 2005-09-21 | AD_VP      | 17000.00 |           0.00 |        100 |            90 |
+-- |         102 | Lex         | De Haan     | LDEHAAN  | 515.123.4569       | 2001-01-13 | AD_VP      | 17000.00 |           0.00 |        100 |            90 |
+-- |         103 | Alexander   | Hunold      | AHUNOLD  | 590.423.4567       | 2006-01-03 | IT_PROG    |  9000.00 |           0.00 |        102 |            60 |
+-- |         104 | Bruce       | Ernst       | BERNST   | 590.423.4568       | 2007-05-21 | IT_PROG    |  6000.00 |           0.00 |        103 |            60 |
+-- |         105 | David       | Austin      | DAUSTIN  | 590.423.4569       | 2005-06-25 | IT_PROG    |  4800.00 |           0.00 |        103 |            60 |
+
+-- Solution
+
+    SELECT first_name, last_name,salary,department_id from employees WHERE salary IN
+    (SELECT MIN(salary) FROM employees group by department_id);
+
+-- Output
+
+-- first_name	last_name	salary	department_id
+-- Neena		Kochhar		17000.00	90
+-- Lex		    De Haan		17000.00	90
+-- Bruce		Ernst		6000.00		60
+-- Diana		Lorentz		4200.00		60
+-- Luis		    Popp		6900.00		100
+
