e0fdb0261dc4312ed9ae5cdfe7923a57ab7e2519
khageshwor <khageshwarjoshi03@gmail.com>
2020-09-05 14:03:24 +0545

    Add problem7 solutions

---
+++ Subquries_HR_DB/problem7.sql
@@ -0,0 +1,31 @@
+-- 7. Write a query to display all the information of an employee whose salary and 
+-- reporting person id is 3000 and 121 respectively
+
+-- Sample table : employees
+
+    --     +-------------+-------------+-------------+----------+--------------------+------------+------------+----------+----------------+------------+---------------+
+-- | EMPLOYEE_ID | FIRST_NAME  | LAST_NAME   | EMAIL    | PHONE_NUMBER       | HIRE_DATE  | JOB_ID     | SALARY   | COMMISSION_PCT | MANAGER_ID | DEPARTMENT_ID |
+-- +-------------+-------------+-------------+----------+--------------------+------------+------------+----------+----------------+------------+---------------+
+-- |         100 | Steven      | King        | SKING    | 515.123.4567       | 2003-06-17 | AD_PRES    | 24000.00 |           0.00 |          0 |            90 |
+-- |         101 | Neena       | Kochhar     | NKOCHHAR | 515.123.4568       | 2005-09-21 | AD_VP      | 17000.00 |           0.00 |        100 |            90 |
+-- |         102 | Lex         | De Haan     | LDEHAAN  | 515.123.4569       | 2001-01-13 | AD_VP      | 17000.00 |           0.00 |        100 |            90 |
+-- |         103 | Alexander   | Hunold      | AHUNOLD  | 590.423.4567       | 2006-01-03 | IT_PROG    |  9000.00 |           0.00 |        102 |            60 |
+-- |         104 | Bruce       | Ernst       | BERNST   | 590.423.4568       | 2007-05-21 | IT_PROG    |  6000.00 |           0.00 |        103 |            60 |
+-- |         105 | David       | Austin      | DAUSTIN  | 590.423.4569       | 2005-06-25 | IT_PROG    |  4800.00 |           0.00 |        103 |            60 |
+
+-- Solution
+
+-- Method-1
+
+    SELECT * from employees WHERE salary = 3000 AND manager_id = 121;
+
+-- Method-2
+
+   -- SELECT * from employees WHERE (salary, manager_id) = (SELECT 3000,121);
+
+-- Output
+
+    -- employee_id	first_name	last_name	email	phone_number	hire_date	job_id	salary	commission_pct	manager_id	department_id
+    --     187		Anthony		Cabrio		ACABRIO	650.509.4876	2007-02-07	SH_CLERK	3000.00	0.00	121		50
+
+
