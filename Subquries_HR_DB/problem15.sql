7ba327088230ff477ae323faa6b313b34ea5313a
khageshwor <khageshwarjoshi03@gmail.com>
2020-09-06 18:36:38 +0545

    Add problem15 Solutions

---
+++ Subquries_HR_DB/problem15.sql
@@ -0,0 +1,34 @@
+-- 15. Write a query to display the employee number, name( first name and last name ), and 
+-- salary for all employees who earn more than the average salary and who work in a department 
+-- with any employee with a J in their name
+
+-- Sample table: employees 
+
+-- Table
+
+-- ---------------+-------------+-------------+----------+--------------------+------------+------------+----------+----------------+------------+---------------+
+-- | EMPLOYEE_ID | FIRST_NAME  | LAST_NAME   | EMAIL    | PHONE_NUMBER       | HIRE_DATE  | JOB_ID     | SALARY   | COMMISSION_PCT | MANAGER_ID | DEPARTMENT_ID |
+-- +-------------+-------------+-------------+----------+--------------------+------------+------------+----------+----------------+------------+---------------+
+-- |         100 | Steven      | King        | SKING    | 515.123.4567       | 2003-06-17 | AD_PRES    | 24000.00 |           0.00 |          0 |            90 |
+-- |         101 | Neena       | Kochhar     | NKOCHHAR | 515.123.4568       | 2005-09-21 | AD_VP      | 17000.00 |           0.00 |        100 |            90 |
+-- |         102 | Lex         | De Haan     | LDEHAAN  | 515.123.4569       | 2001-01-13 | AD_VP      | 17000.00 |           0.00 |        100 |            90 |
+-- |         103 | Alexander   | Hunold      | AHUNOLD  | 590.423.4567       | 2006-01-03 | IT_PROG    |  9000.00 |           0.00 |        102 |            60 |
+-- |         104 | Bruce       | Ernst       | BERNST   | 590.423.4568       | 2007-05-21 | IT_PROG    |  6000.00 |           0.00 |        103 |            60 |
+-- |         105 | David       | Austin      | DAUSTIN  | 590.423.4569       | 2005-06-25 | IT_PROG    |  4800.00 |           0.00 |        103 |            60 |
+-- AND so on
+
+-- Solution
+
+-- Method-1
+
+    SELECT employee_id, first_name, last_name, salary FROM employees WHERE department_id IN ( SELECT department_id FROM employees WHERE first_name LIKE '%J%' )AND salary > (SELECT avg(salary) FROM employees);
+
+-- Output
+
+-- employee_id	first_name	salary
+-- 108		Nancy		12000.00
+-- 109		Daniel		9000.00
+-- 110		John		8200.00
+-- 111		Ismael		7700.00
+-- 112		Jose Manuel	7800.00
+-- 113		Luis		6900.00
