152ca2af6ac191d318acb950d6d19071ee1e3084
khageshwor <khageshwarjoshi03@gmail.com>
2020-09-06 18:19:23 +0545

    Add problem13 Solutions

---
+++ Subquries_HR_DB/problem13.sql
@@ -0,0 +1,26 @@
+-- 13. Write a query to display the employee name( first name and last name ) and 
+-- hiredate for all employees in the same department as Clara. Exclude Clara. 
+
+-- Sample table: employees 
+
+-- +-------------+-------------+-------------+----------+--------------------+------------+------------+----------+----------------+------------+---------------+
+-- | EMPLOYEE_ID | FIRST_NAME  | LAST_NAME   | EMAIL    | PHONE_NUMBER       | HIRE_DATE  | JOB_ID     | SALARY   | COMMISSION_PCT | MANAGER_ID | DEPARTMENT_ID |
+-- +-------------+-------------+-------------+----------+--------------------+------------+------------+----------+----------------+------------+---------------+
+-- |         100 | Steven      | King        | SKING    | 515.123.4567       | 2003-06-17 | AD_PRES    | 24000.00 |           0.00 |          0 |            90 |
+-- |         101 | Neena       | Kochhar     | NKOCHHAR | 515.123.4568       | 2005-09-21 | AD_VP      | 17000.00 |           0.00 |        100 |            90 |
+-- |         102 | Lex         | De Haan     | LDEHAAN  | 515.123.4569       | 2001-01-13 | AD_VP      | 17000.00 |           0.00 |        100 |            90 |
+-- |         103 | Alexander   | Hunold      | AHUNOLD  | 590.423.4567       | 2006-01-03 | IT_PROG    |  9000.00 |           0.00 |        102 |            60 |
+-- |         104 | Bruce       | Ernst       | BERNST   | 590.423.4568       | 2007-05-21 | IT_PROG    |  6000.00 |           0.00 |        103 |            60 |
+-- |         105 | David       | Austin      | DAUSTIN  | 590.423.4569       | 2005-06-25 | IT_PROG    |  4800.00 |           0.00 |        103 |            60 |
+
+-- Solution
+
+-- Method-1
+
+    SELECT first_name, last_name, hire_date FROM employees WHERE department_id = (SELECT department_id FROM employees WHERE first_name = 'Clara') AND first_name <> 'Clara'
+
+-- But it is recommended to use IN operator instead of '='
+
+   SELECT * FROM employees WHERE department_id IN (
+   SELECT department_id FROM employees WHERE first_name = 'Clara' ) AND first_name <> 'Clara';
+
