2d811fdeb514148ab7cc4fac64f3f3d5e9f9c0bd
khageshwor <khageshwarjoshi03@gmail.com>
2020-09-12 18:24:06 +0545

    Add problem21 solution

---
+++ Subquries_HR_DB/problem21.sql
@@ -0,0 +1,33 @@
+-- 21. Write a query to display the employee name( first name and last name ) and 
+-- department for all employees for any existence of those employees whose salary is more than 3700. 
+
+-- Sample table: employees
+
+-- Table
+
+-- ---------------+-------------+-------------+----------+--------------------+------------+------------+----------+----------------+------------+---------------+
+-- | EMPLOYEE_ID | FIRST_NAME  | LAST_NAME   | EMAIL    | PHONE_NUMBER       | HIRE_DATE  | JOB_ID     | SALARY   | COMMISSION_PCT | MANAGER_ID | DEPARTMENT_ID |
+-- +-------------+-------------+-------------+----------+--------------------+------------+------------+----------+----------------+------------+---------------+
+-- |         100 | Steven      | King        | SKING    | 515.123.4567       | 2003-06-17 | AD_PRES    | 24000.00 |           0.00 |          0 |            90 |
+-- |         101 | Neena       | Kochhar     | NKOCHHAR | 515.123.4568       | 2005-09-21 | AD_VP      | 17000.00 |           0.00 |        100 |            90 |
+-- |         102 | Lex         | De Haan     | LDEHAAN  | 515.123.4569       | 2001-01-13 | AD_VP      | 17000.00 |           0.00 |        100 |            90 |
+-- |         103 | Alexander   | Hunold      | AHUNOLD  | 590.423.4567       | 2006-01-03 | IT_PROG    |  9000.00 |           0.00 |        102 |            60 |
+-- |         104 | Bruce       | Ernst       | BERNST   | 590.423.4568       | 2007-05-21 | IT_PROG    |  6000.00 |           0.00 |        103 |            60 |
+-- |         105 | David       | Austin      | DAUSTIN  | 590.423.4569       | 2005-06-25 | IT_PROG    |  4800.00 |           0.00 |        103 |            60 |
+-- AND so on
+
+-- Solution
+
+-- Method-1
+
+    SELECT first_name, last_name, department_id 
+    FROM employees WHERE Exists (SELECT * from employees WHERE salary > 3700);
+
+-- Output
+
+-- first_name	last_name	department_id
+-- Steven		King		90
+-- Neena		Kochhar		90
+-- Lex		    De Haan		90
+-- Alexander	Hunold		60
+-- Bruce		Ernst		60
