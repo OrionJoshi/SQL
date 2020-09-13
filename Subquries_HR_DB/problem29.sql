f9d11d3f699ebf2f8a4d6c18d563aa0228479e42
khageshwor <khageshwarjoshi03@gmail.com>
2020-09-13 18:22:04 +0545

    Add problem29 solutions

---
+++ Subquries_HR_DB/problem29.sql
@@ -0,0 +1,33 @@
+-- 29. Write a query to determine who earns more than Mr. Ozer
+
+-- Sample table: employees
+
+-- Table
+
+-- ---------------+-------------+-------------+----------+--------------------+------------+------------+----------+----------------+------------+---------------+
+-- | EMPLOYEE_ID | FIRST_NAME  | LAST_NAME   | EMAIL    | PHONE_NUMBER       | HIRE_DATE  | JOB_ID     | SALARY   | COMMISSION_PCT | MANAGER_ID | DEPARTMENT_ID |
+-- +-------------+-------------+-------------+----------+--------------------+------------+------------+----------+----------------+------------+---------------+
+-- |         100 | Steven      | King        | SKING    | 515.123.4567       | 2003-06-17 | AD_PRES    | 24000.00 |           0.00 |          0 |            90 |
+-- |         101 | Neena       | Kochhar     | NKOCHHAR | 515.123.4568       | 2005-09-21 | AD_VP      | 17000.00 |           0.00 |        100 |            90 |
+-- |         102 | Lex         | De Haan     | LDEHAAN  | 515.123.4569       | 2001-01-13 | AD_VP      | 17000.00 |           0.00 |        100 |            90 |
+-- |         103 | Alexander   | Hunold      | AHUNOLD  | 590.423.4567       | 2006-01-03 | IT_PROG    |  9000.00 |           0.00 |        102 |            60 |
+-- |         104 | Bruce       | Ernst       | BERNST   | 590.423.4568       | 2007-05-21 | IT_PROG    |  6000.00 |           0.00 |        103 |            60 |
+-- |         105 | David       | Austin      | DAUSTIN  | 590.423.4569       | 2005-06-25 | IT_PROG    |  4800.00 |           0.00 |        103 |            60 |
+-- AND so on
+
+-- Solutions
+
+-- Method-1
+
+    SELECT employee_id, first_name, last_name, salary 
+    FROM employees WHERE salary > (SELECT salary FROM employees WHERE last_name = 'Ozer');
+
+-- Output
+
+-- first_name	last_name	salary
+-- Lex		    De Haan		17000.00
+-- Alberto		Errazuriz	12000.00
+-- Nancy		Greenberg	12000.00
+-- Michael		Hartstein	13000.00
+-- Shelley		Higgins		12000.00
+ 
