b930ee23d88bbe9cdfe49b850a1623129c03d08d
khageshwor <khageshwarjoshi03@gmail.com>
2020-09-13 18:17:38 +0545

    Add problem28 solutions

---
+++ Subquries_HR_DB/problem28.sql
@@ -0,0 +1,60 @@
+-- 28. Write a query to identify all the employees who earn more than the average and 
+-- who work in any of the IT departments.
+
+
+-- Sample table: employees
+
+-- Table
+
+-- ---------------+-------------+-------------+----------+--------------------+------------+------------+----------+----------------+------------+---------------+
+-- | EMPLOYEE_ID | FIRST_NAME  | LAST_NAME   | EMAIL    | PHONE_NUMBER       | HIRE_DATE  | JOB_ID     | SALARY   | COMMISSION_PCT | MANAGER_ID | DEPARTMENT_ID |
+-- +-------------+-------------+-------------+----------+--------------------+------------+------------+----------+----------------+------------+---------------+
+-- |         100 | Steven      | King        | SKING    | 515.123.4567       | 2003-06-17 | AD_PRES    | 24000.00 |           0.00 |          0 |            90 |
+-- |         101 | Neena       | Kochhar     | NKOCHHAR | 515.123.4568       | 2005-09-21 | AD_VP      | 17000.00 |           0.00 |        100 |            90 |
+-- |         102 | Lex         | De Haan     | LDEHAAN  | 515.123.4569       | 2001-01-13 | AD_VP      | 17000.00 |           0.00 |        100 |            90 |
+-- |         103 | Alexander   | Hunold      | AHUNOLD  | 590.423.4567       | 2006-01-03 | IT_PROG    |  9000.00 |           0.00 |        102 |            60 |
+-- |         104 | Bruce       | Ernst       | BERNST   | 590.423.4568       | 2007-05-21 | IT_PROG    |  6000.00 |           0.00 |        103 |            60 |
+-- |         105 | David       | Austin      | DAUSTIN  | 590.423.4569       | 2005-06-25 | IT_PROG    |  4800.00 |           0.00 |        103 |            60 |
+-- AND so on
+
+-- Sample table: departments
+
+--     +---------------+----------------------+------------+-------------+
+-- | DEPARTMENT_ID | DEPARTMENT_NAME      | MANAGER_ID | LOCATION_ID |
+-- +---------------+----------------------+------------+-------------+
+-- |            10 | Administration       |        200 |        1700 |
+-- |            20 | Marketing            |        201 |        1800 |
+-- |            30 | Purchasing           |        114 |        1700 |
+-- |            40 | Human Resources      |        203 |        2400 |
+
+
+-- Solutions
+
+-- Method-1
+
+    SELECT * from employees WHERE salary > (SELECT AVG(salary) FROM employees) 
+    AND department_id = (SELECT department_id FROM departments WHERE department_name = 'IT')
+
+-- Method-2
+
+    SELECT * from employees e 
+    JOIN departments d ON e.department_id = d.department_id 
+    WHERE salary > (SELECT AVG(salary) FROM employees) AND department_name = 'IT';
+
+-- Method-3
+
+    SELECT last_name 
+    FROM employees 
+    WHERE department_id IN 
+    (SELECT department_id 
+    FROM departments 
+    WHERE department_name LIKE 'IT%') 
+    AND salary > 
+    (SELECT avg(salary) 
+    FROM employees);
+
+-- Output
+
+    -- last_name
+    -- Hunold
+
