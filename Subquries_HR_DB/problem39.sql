04a59d97b9698a1c9250138e65a9a3b138f8569a
khageshwor <khageshwarjoshi03@gmail.com>
2020-09-14 21:19:16 +0545

    Add problem39 Solution

---
+++ Subquries_HR_DB/problem39.sql
@@ -0,0 +1,32 @@
+-- 39. Write a query in SQL to display the first and last name, salary, and 
+-- department ID for all those employees who work in that department where
+-- the employee works who hold the ID 201.
+
+-- Sample table: employees
+
+-- Table
+
+-- ---------------+-------------+-------------+----------+--------------------+------------+------------+----------+----------------+------------+---------------+
+-- | EMPLOYEE_ID | FIRST_NAME  | LAST_NAME   | EMAIL    | PHONE_NUMBER       | HIRE_DATE  | JOB_ID     | SALARY   | COMMISSION_PCT | MANAGER_ID | DEPARTMENT_ID |
+-- +-------------+-------------+-------------+----------+--------------------+------------+------------+----------+----------------+------------+---------------+
+-- |         100 | Steven      | King        | SKING    | 515.123.4567       | 2003-06-17 | AD_PRES    | 24000.00 |           0.00 |          0 |            90 |
+-- |         101 | Neena       | Kochhar     | NKOCHHAR | 515.123.4568       | 2005-09-21 | AD_VP      | 17000.00 |           0.00 |        100 |            90 |
+-- |         102 | Lex         | De Haan     | LDEHAAN  | 515.123.4569       | 2001-01-13 | AD_VP      | 17000.00 |           0.00 |        100 |            90 |
+-- |         103 | Alexander   | Hunold      | AHUNOLD  | 590.423.4567       | 2006-01-03 | IT_PROG    |  9000.00 |           0.00 |        102 |            60 |
+-- |         104 | Bruce       | Ernst       | BERNST   | 590.423.4568       | 2007-05-21 | IT_PROG    |  6000.00 |           0.00 |        103 |            60 |
+-- |         105 | David       | Austin      | DAUSTIN  | 590.423.4569       | 2005-06-25 | IT_PROG    |  4800.00 |           0.00 |        103 |            60 |
+-- AND so on
+
+
+-- Solutions
+
+-- Method-1
+
+    SELECT first_name, last_name, salary, department_id 
+    FROM employees WHERE department_id = (SELECT department_id FROM employees WHERE employee_id = 201)
+
+-- Output
+
+-- first_name	last_name	salary	department_id
+-- Michael		Hartstein	13000.00	20
+-- Pat		    Fay		    6000.00		20
