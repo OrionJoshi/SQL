643a8b3f740b5056935d9b5be1f624ddceb60c1a
khageshwor <khageshwarjoshi03@gmail.com>
2020-09-15 20:45:07 +0545

    Add problem46 solutions

---
+++ Subquries_HR_DB/problem46.sql
@@ -0,0 +1,60 @@
+-- 46. Write a query in SQL to display the first and last name, salary and 
+-- department ID for those employees whose department is located in the city London. 
+
+-- Sample table: employees
+
+-- Table
+
+-- ---------------+-------------+-------------+----------+--------------------+------------+------------+----------+----------------+------------+---------------+
+-- | EMPLOYEE_ID | FIRST_NAME  | LAST_NAME   | EMAIL    | PHONE_NUMBER       | HIRE_DATE  | JOB_ID     | SALARY   | COMMISSION_PCT | MANAGER_ID | DEPARTMENT_ID |
+-- +-------------+-------------+-------------+----------+--------------------+------------+------------+----------+----------------+------------+---------------+
+-- |         100 | Steven      | King        | SKING    | 515.123.4567       | 2003-06-17 | AD_PRES    | 24000.00 |           0.00 |          0 |            90 |
+-- |         101 | Neena       | Kochhar     | NKOCHHAR | 515.123.4568       | 2005-09-21 | AD_VP      | 17000.00 |           0.00 |        100 |            90 |
+-- |         102 | Lex         | De Haan     | LDEHAAN  | 515.123.4569       | 2001-01-13 | AD_VP      | 17000.00 |           0.00 |        100 |            90 |
+-- |         103 | Alexander   | Hunold      | AHUNOLD  | 590.423.4567       | 2006-01-03 | IT_PROG    |  9000.00 |           0.00 |        102 |            60 |
+-- |         104 | Bruce       | Ernst       | BERNST   | 590.423.4568       | 2007-05-21 | IT_PROG    |  6000.00 |           0.00 |        103 |            60 |
+-- |         105 | David       | Austin      | DAUSTIN  | 590.423.4569       | 2005-06-25 | IT_PROG    |  4800.00 |           0.00 |        103 |            60 |
+-- AND so on
+
+-- Sample table: departments
+
+--     +---------------+----------------------+------------+-------------+
+-- | DEPARTMENT_ID | DEPARTMENT_NAME      | MANAGER_ID | LOCATION_ID |
+-- +---------------+----------------------+------------+-------------+
+-- |            10 | Administration       |        200 |        1700 |
+-- |            20 | Marketing            |        201 |        1800 |
+-- |            30 | Purchasing           |        114 |        1700 |
+-- |            40 | Human Resources      |        203 |        2400 |
+-- |            50 | Shipping             |        121 |        1500 |
+
+
+-- Sample table: locations
+
+--     +-------------+------------------------------------------+-------------+---------------------+-------------------+------------+
+-- | LOCATION_ID | STREET_ADDRESS                           | POSTAL_CODE | CITY                | STATE_PROVINCE    | COUNTRY_ID |
+-- +-------------+------------------------------------------+-------------+---------------------+-------------------+------------+
+-- |        1000 | 1297 Via Cola di Rie                     | 989         | Roma                |                   | IT         |
+-- |        1100 | 93091 Calle della Testa                  | 10934       | Venice              |                   | IT         |
+-- |        1200 | 2017 Shinjuku-ku                         | 1689        | Tokyo               | Tokyo Prefecture  | JP         |
+-- |        1300 | 9450 Kamiya-cho                          | 6823        | Hiroshima           |                   | JP         |
+
+
+
+-- Solutions
+
+-- Method-1
+
+    SELECT first_name, last_name, salary, department_id 
+    FROM employees
+    WHERE department_id IN (SELECT department_id FROM departments WHERE location_id = (SELECT location_id FROM locations 
+    WHERE city = 'London'));
+
+-- Method-2
+
+    select CONCAT(e.first_name, e.last_name) as "name", e.salary, e.department_id
+    from employees e
+    join departments d
+    on e.department_id = d.department_id
+    join locations l
+    on l.location_id = d.location_id
+    where l.city='London'
