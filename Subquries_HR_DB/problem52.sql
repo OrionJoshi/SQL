14410fe1cdf292c18a41e42f556a753c64d3ac98
khageshwor <khageshwarjoshi03@gmail.com>
2020-09-16 22:00:16 +0545

    Add problem52 Solutions

---
+++ Subquries_HR_DB/problem52.sql
@@ -0,0 +1,24 @@
+-- 52. Write a query in SQL to display all the infromation about those employees
+-- who earn second lowest salary of all the employees.
+
+-- Sample table: employees
+
+--     +-------------+-------------+-------------+----------+--------------------+------------+------------+----------+----------------+------------+---------------+
+-- | EMPLOYEE_ID | FIRST_NAME  | LAST_NAME   | EMAIL    | PHONE_NUMBER       | HIRE_DATE  | JOB_ID     | SALARY   | COMMISSION_PCT | MANAGER_ID | DEPARTMENT_ID |
+-- +-------------+-------------+-------------+----------+--------------------+------------+------------+----------+----------------+------------+---------------+
+-- |         100 | Steven      | King        | SKING    | 515.123.4567       | 2003-06-17 | AD_PRES    | 24000.00 |           0.00 |          0 |            90 |
+-- |         101 | Neena       | Kochhar     | NKOCHHAR | 515.123.4568       | 2005-09-21 | AD_VP      | 17000.00 |           0.00 |        100 |            90 |
+-- |         102 | Lex         | De Haan     | LDEHAAN  | 515.123.4569       | 2001-01-13 | AD_VP      | 17000.00 |           0.00 |        100 |            90 |
+-- |         103 | Alexander   | Hunold      | AHUNOLD  | 590.423.4567       | 2006-01-03 | IT_PROG    |  9000.00 |           0.00 |        102 |            60 |
+-- |         104 | Bruce       | Ernst       | BERNST   | 590.423.4568       | 2007-05-21 | IT_PROG    |  6000.00 |           0.00 |        103 |            60 |
+
+-- Solutions
+
+-- Method-1
+
+    -- SELECT * FROM employees e WHERE e.salary > (SELECT MIN(salary) FROM employees) ORDER BY salary LIMIT 2
+
+-- Method-2
+
+    SELECT * FROM employees m WHERE  2 = (SELECT COUNT(DISTINCT salary ) FROM employees WHERE  salary <= m.salary);
+
