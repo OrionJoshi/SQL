44bc9f37deaa0049cfc04a9c6a5a7935aff3e1da
khageshwor <khageshwarjoshi03@gmail.com>
2020-09-17 18:04:52 +0545

    Add problem8 solutions

---
+++ Joins_HR_DB/problem8.sql
@@ -0,0 +1,38 @@
+-- 8. Write a query in SQL to display the first name of all employees including the first name of their manager. 
+
+
+-- Sample table: employees
+
+--     +-------------+-------------+-------------+----------+--------------------+------------+------------+----------+----------------+------------+---------------+
+-- | EMPLOYEE_ID | FIRST_NAME  | LAST_NAME   | EMAIL    | PHONE_NUMBER       | HIRE_DATE  | JOB_ID     | SALARY   | COMMISSION_PCT | MANAGER_ID | DEPARTMENT_ID |
+-- +-------------+-------------+-------------+----------+--------------------+------------+------------+----------+----------------+------------+---------------+
+-- |         100 | Steven      | King        | SKING    | 515.123.4567       | 2003-06-17 | AD_PRES    | 24000.00 |           0.00 |          0 |            90 |
+-- |         101 | Neena       | Kochhar     | NKOCHHAR | 515.123.4568       | 2005-09-21 | AD_VP      | 17000.00 |           0.00 |        100 |            90 |
+-- |         102 | Lex         | De Haan     | LDEHAAN  | 515.123.4569       | 2001-01-13 | AD_VP      | 17000.00 |           0.00 |        100 |            90 |
+-- |         103 | Alexander   | Hunold      | AHUNOLD  | 590.423.4567       | 2006-01-03 | IT_PROG    |  9000.00 |           0.00 |        102 |            60 |
+
+-- Sample table: locations
+
+--     +-------------+------------------------------------------+-------------+---------------------+-------------------+------------+
+-- | LOCATION_ID | STREET_ADDRESS                           | POSTAL_CODE | CITY                | STATE_PROVINCE    | COUNTRY_ID |
+-- +-------------+------------------------------------------+-------------+---------------------+-------------------+------------+
+-- |        1000 | 1297 Via Cola di Rie                     | 989         | Roma                |                   | IT         |
+-- |        1100 | 93091 Calle della Testa                  | 10934       | Venice              |                   | IT         |
+-- |        1200 | 2017 Shinjuku-ku                         | 1689        | Tokyo               | Tokyo Prefecture  | JP         |
+
+-- Solutions
+
+-- Method-1
+
+    SELECT E.first_name AS Employee_Name, M.first_name AS Manager_name 
+    FROM employees E JOIN employees M ON E.manager_id = M.employee_id
+
+-- Output
+
+-- Employee Name	Manager
+-- Neena		Steven
+-- Lex		    Steven
+-- Alexander	Lex
+-- Bruce		Alexander
+-- David		Alexander
+
