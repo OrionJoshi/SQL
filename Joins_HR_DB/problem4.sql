887d57c16b91ea941e194b4e6c28ba2b3871a360
khageshwor <khageshwarjoshi03@gmail.com>
2020-09-17 17:53:49 +0545

    Add problem4 solutions

---
+++ Joins_HR_DB/problem4.sql
@@ -0,0 +1,37 @@
+-- 4. Write a query in SQL to display the first name, last name, department number 
+-- and department name, for all employees for departments 80 or 40.
+
+-- Sample table: departments
+
+--     +---------------+----------------------+------------+-------------+
+-- | DEPARTMENT_ID | DEPARTMENT_NAME      | MANAGER_ID | LOCATION_ID |
+-- +---------------+----------------------+------------+-------------+
+-- |            10 | Administration       |        200 |        1700 |
+-- |            20 | Marketing            |        201 |        1800 |
+-- |            30 | Purchasing           |        114 |        1700 |
+-- |            40 | Human Resources      |        203 |        2400 |
+
+
+-- Sample table: employees
+
+--     +-------------+-------------+-------------+----------+--------------------+------------+------------+----------+----------------+------------+---------------+
+-- | EMPLOYEE_ID | FIRST_NAME  | LAST_NAME   | EMAIL    | PHONE_NUMBER       | HIRE_DATE  | JOB_ID     | SALARY   | COMMISSION_PCT | MANAGER_ID | DEPARTMENT_ID |
+-- +-------------+-------------+-------------+----------+--------------------+------------+------------+----------+----------------+------------+---------------+
+-- |         100 | Steven      | King        | SKING    | 515.123.4567       | 2003-06-17 | AD_PRES    | 24000.00 |           0.00 |          0 |            90 |
+-- |         101 | Neena       | Kochhar     | NKOCHHAR | 515.123.4568       | 2005-09-21 | AD_VP      | 17000.00 |           0.00 |        100 |            90 |
+-- |         102 | Lex         | De Haan     | LDEHAAN  | 515.123.4569       | 2001-01-13 | AD_VP      | 17000.00 |           0.00 |        100 |            90 |
+-- |         103 | Alexander   | Hunold      | AHUNOLD  | 590.423.4567       | 2006-01-03 | IT_PROG    |  9000.00 |           0.00 |        102 |            60 |
+
+-- Solutions
+
+-- Method-1
+
+    SELECT e.first_name, e.last_name, d.department_id, d.department_name 
+    FROM employees e JOIN departments d ON e.department_id = d.department_id 
+    WHERE d.department_id IN (80, 40)
+
+-- Method-2
+
+    SELECT e.first_name, e.last_name, d.department_id, d.department_name 
+    FROM employees e JOIN departments d ON e.department_id = d.department_id 
+    WHERE d.department_id = 80 OR d.department_id = 40
