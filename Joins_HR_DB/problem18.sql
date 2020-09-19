3795c954aa609fe95a06534c55363f987393edc9
khageshwor <khageshwarjoshi03@gmail.com>
2020-09-19 11:42:12 +0545

    Add problem18 Solution

---
+++ Joins_HR_DB/problem18.sql
@@ -0,0 +1,38 @@
+-- 18. Write a query in SQL to display department name and the full name (first and last name) of the manager.
+
+-- Sample table: departments
+
+--     +---------------+----------------------+------------+-------------+
+-- | DEPARTMENT_ID | DEPARTMENT_NAME      | MANAGER_ID | LOCATION_ID |
+-- +---------------+----------------------+------------+-------------+
+-- |            10 | Administration       |        200 |        1700 |
+-- |            20 | Marketing            |        201 |        1800 |
+-- |            30 | Purchasing           |        114 |        1700 |
+-- |            40 | Human Resources      |        203 |        2400 |
+
+
+-- Sample table: employees
+
+--     +-------------+-------------+-------------+----------+--------------------+------------+------------+----------+----------------+------------+---------------+
+-- | EMPLOYEE_ID | FIRST_NAME  | LAST_NAME   | EMAIL    | PHONE_NUMBER       | HIRE_DATE  | JOB_ID     | SALARY   | COMMISSION_PCT | MANAGER_ID | DEPARTMENT_ID |
+-- +-------------+-------------+-------------+----------+--------------------+------------+------------+----------+----------------+------------+---------------+
+-- |         100 | Steven      | King        | SKING    | 515.123.4567       | 2003-06-17 | AD_PRES    | 24000.00 |           0.00 |          0 |            90 |
+-- |         101 | Neena       | Kochhar     | NKOCHHAR | 515.123.4568       | 2005-09-21 | AD_VP      | 17000.00 |           0.00 |        100 |            90 |
+-- |         102 | Lex         | De Haan     | LDEHAAN  | 515.123.4569       | 2001-01-13 | AD_VP      | 17000.00 |           0.00 |        100 |            90 |
+-- |         103 | Alexander   | Hunold      | AHUNOLD  | 590.423.4567       | 2006-01-03 | IT_PROG    |  9000.00 |           0.00 |        102 |            60 |
+
+-- Solutions
+
+-- Method-1
+
+    SELECT d.department_name, CONCAT(e.first_name, ' ', e.last_name) AS name_of_manager 
+    FROM departments d JOIN employees e ON e.employee_id = d.manager_id
+
+-- Output
+
+-- department_name	name_of_manager
+-- Executive	Steven King
+-- IT		    Alexander Hunold
+-- Finance		Nancy Greenberg
+-- Purchasing	Den Raphaely
+
