ff3e148ecf4402409d63379a94a4fe6956ea9a00
khageshwor <khageshwarjoshi03@gmail.com>
2020-09-24 21:26:18 +0545

    Add problem2 Solutions

---
+++ Wildcard-special-operators/problem2.sql
@@ -0,0 +1,35 @@
+-- 2. Write a query to filter those salesmen with all information who comes 
+-- from any of the cities Paris and Rome
+
+-- Sample table: salesman
+
+-- salesman_id |    name    |   city   | commission 
+-- -------------+------------+----------+------------
+--         5001 | James Hoog | New York |       0.15
+--         5002 | Nail Knite | Paris    |       0.13
+--         5005 | Pit Alex   | London   |       0.11
+--         5006 | Mc Lyon    | Paris    |       0.14
+
+
+-- Solutions
+
+-- Method-1
+
+    SELECT * 
+    FROM salesman 
+    WHERE city 
+    IN('Paris','Rome');
+
+-- Method-2
+
+    SELECT * 
+    FROM salesman 
+    WHERE city ='Paris' 
+    OR city='Rome';
+
+-- Output
+
+-- salesman_id	name		city	commission
+-- 5002		Nail Knite	Paris	0.13
+-- 5006		Mc Lyon		Paris	0.14
+-- 5007		Paul Adam	Rome	0.13
