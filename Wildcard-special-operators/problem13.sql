c3751411e1ddc153d386da905b5ce2da9ff02b70
khageshwor <khageshwarjoshi03@gmail.com>
2020-09-25 20:54:45 +0545

    Add problem13 Solutions

---
+++ Wildcard-special-operators/problem13.sql
@@ -0,0 +1,27 @@
+-- 13. Write a SQL statement to find those rows from the table testtable which does not 
+-- contain the character underscore ( _ ) in its column 'col1'.
+
+-- Sample table: testtable
+
+
+-- col1
+-- --------------------------
+-- A001/DJ-402\44_/100/2015
+-- A001_\DJ-402\44_/100/2015
+-- A001_DJ-402-2014-2015
+-- A002_DJ-401-2014-2015
+-- A001/DJ_401
+
+-- Solutions
+
+-- Method-1
+
+    SELECT *
+    FROM testtable
+    WHERE col1 NOT LIKE '%/_%' ESCAPE '/';
+
+    -- Output
+
+--         col1
+-- A001/DJ-402%45\2015/200
+-- A001/DJ-402\44
