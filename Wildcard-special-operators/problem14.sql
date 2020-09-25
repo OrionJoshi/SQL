17900be74a2a0675bbc4203394f7a8511cc6708a
khageshwor <khageshwarjoshi03@gmail.com>
2020-09-25 20:58:25 +0545

    Add problem14 Solutions

---
+++ Wildcard-special-operators/problem14.sql
@@ -0,0 +1,28 @@
+-- 14. Write a SQL statement to find those rows from the table testtable which contain the 
+-- escape character ( / ) in its column 'col1'.
+
+-- Sample table: testtable
+
+
+-- col1
+-- --------------------------
+-- A001/DJ-402\44_/100/2015
+-- A001_\DJ-402\44_/100/2015
+-- A001_DJ-402-2014-2015
+-- A002_DJ-401-2014-2015
+-- A001/DJ_401
+
+-- Solutions
+
+-- Method-1
+
+    SELECT *
+    FROM testtable
+    WHERE col1 LIKE '%//%' ESCAPE '/';
+
+-- Output
+
+--     col1
+-- A001/DJ-402\44_/100/2015
+-- A001_\DJ-402\44_/100/2015
+-- A001/DJ_401
