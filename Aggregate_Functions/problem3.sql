aa4eb09214d4627a5e07f45c330e4dc0a0b9c579
khageshwor <khageshwarjoshi03@gmail.com>
2020-09-26 21:40:53 +0545

    Add problem3 Solution

---
+++ Aggregate_Functions/problem3.sql
@@ -0,0 +1,21 @@
+-- 3. Write a SQL statement to find the number of salesmen currently listing for all of their customers.
+
+-- Sample table: orders 
+
+--     ord_no      purch_amt   ord_date    customer_id  salesman_id
+-- ----------  ----------  ----------  -----------  -----------
+-- 70001       150.5       2012-10-05  3005         5002
+-- 70009       270.65      2012-09-10  3001         5005
+-- 70002       65.26       2012-10-05  3002         5001
+
+-- Solutions
+
+-- Method-1
+
+    SELECT COUNT (DISTINCT salesman_id) 
+    FROM orders;
+
+-- Output
+
+-- count
+-- 6
