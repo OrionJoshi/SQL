2c73fb4dd93d229602a704afa5da67c53d23a95c
khageshwor <khageshwarjoshi03@gmail.com>
2020-10-04 21:34:28 +0545

    Add problem13 Solution

---
+++ Sql_View/problem13.sql
@@ -0,0 +1,37 @@
+-- 13. Write a query to create a view that shows all matches of customers with salesman
+--  such that at least one customer in the city of customer served by a salesman in the city
+--   of the salesman.
+
+-- Sample table: customer
+
+--     customer_id |   cust_name    |    city    | grade | salesman_id 
+-- -------------+----------------+------------+-------+-------------
+--         3002 | Nick Rimando   | New York   |   100 |        5001
+--         3007 | Brad Davis     | New York   |   200 |        5001
+--         3005 | Graham Zusi    | California |   200 |        5002
+--         3008 | Julian Green   | London     |   300 |        500
+
+--Sample table: salesman
+
+-- salesman_id |    name    |   city   | commission 
+-- -------------+------------+----------+------------
+--         5001 | James Hoog | New York |       0.15
+--         5002 | Nail Knite | Paris    |       0.13
+--         5005 | Pit Alex   | London   |       0.11
+ 
+-- Solution
+
+-- Method-1
+
+    CREATE VIEW city_same
+    AS SELECT DISTINCT a.city, b.city
+    FROM customer a, salesman b
+    WHERE a.salesman_id = b.salesman_id;
+
+-- Output
+
+    --       custcity  | salescity
+    -- ------------+-----------
+    --  Seattle    | Paris
+    --  Moscow     | Rome
+    --  New York   | New York
