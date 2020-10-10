e42333e93c93acc4f0e0c96d4fca7e6fbf930783
khageshwor <khageshwarjoshi03@gmail.com>
2020-10-10 22:15:03 +0545

    Add problem11 Solutions

---
+++ Sorting_and_Filtering_HR/problem11.sql
@@ -0,0 +1,29 @@
+-- 11. Write a query in SQL to display the full name (first and last) name, and salary, for all employees
+-- whose salary is out of the range 7000 and 15000 and make the result set in ascending order by the full name.
+
+-- Sample table: employees
+
+--     +-------------+-------------+-------------+----------+--------------------+------------+------------+----------+----------------+------------+---------------+
+-- | EMPLOYEE_ID | FIRST_NAME  | LAST_NAME   | EMAIL    | PHONE_NUMBER       | HIRE_DATE  | JOB_ID     | SALARY   | COMMISSION_PCT | MANAGER_ID | DEPARTMENT_ID |
+-- +-------------+-------------+-------------+----------+--------------------+------------+------------+----------+----------------+------------+---------------+
+-- |         100 | Steven      | King        | SKING    | 515.123.4567       | 2003-06-17 | AD_PRES    | 24000.00 |           0.00 |          0 |            90 |
+-- |         101 | Neena       | Kochhar     | NKOCHHAR | 515.123.4568       | 2005-09-21 | AD_VP      | 17000.00 |           0.00 |        100 |            90 |
+-- |         102 | Lex         | De Haan     | LDEHAAN  | 515.123.4569       | 2001-01-13 | AD_VP      | 17000.00 |           0.00 |        100 |            90 |
+-- |         103 | Alexander   | Hunold      | AHUNOLD  | 590.423.4567       | 2006-01-03 | IT_PROG    |  9000.00 |           0.00 |        102 |            60 |
+
+-- Solutions
+
+-- Method-1
+
+    SELECT CONCAT(first_name, ' ', last_name) as Name, salary
+    FROM  employees
+    WHERE salary NOT BETWEEN 7000 AND 15000
+    ORDER BY Name;
+
+-- Output 
+
+    --     name        |  salary
+    -- -------------------+----------
+    -- Alana Walsh       |  3100.00
+    -- Alexander Khoo    |  3100.00
+    -- Alexis Bull       |  4100.00
