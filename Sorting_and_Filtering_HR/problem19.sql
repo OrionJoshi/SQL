14a93216158609a65ee7118fdd8b3e563a27e3de
khageshwor <khageshwarjoshi03@gmail.com>
2020-10-11 21:26:45 +0545

    Add problem19 Solutions

---
+++ Sorting_and_Filtering_HR/problem19.sql
@@ -0,0 +1,27 @@
+-- 19. Write a query in SQL to display the first and last name, and department number for those employees
+--  who holds a letter s as a 3rd character in their first name.
+
+-- Sample table: employees
+
+--     +-------------+-------------+-------------+----------+--------------------+------------+------------+----------+----------------+------------+---------------+
+-- | EMPLOYEE_ID | FIRST_NAME  | LAST_NAME   | EMAIL    | PHONE_NUMBER       | HIRE_DATE  | JOB_ID     | SALARY   | COMMISSION_PCT | MANAGER_ID | DEPARTMENT_ID |
+-- +-------------+-------------+-------------+----------+--------------------+------------+------------+----------+----------------+------------+---------------+
+-- |         100 | Steven      | King        | SKING    | 515.123.4567       | 2003-06-17 | AD_PRES    | 24000.00 |           0.00 |          0 |            90 |
+-- |         101 | Neena       | Kochhar     | NKOCHHAR | 515.123.4568       | 2005-09-21 | AD_VP      | 17000.00 |           0.00 |        100 |            90 |
+-- |         102 | Lex         | De Haan     | LDEHAAN  | 515.123.4569       | 2001-01-13 | AD_VP      | 17000.00 |           0.00 |        100 |            90 |
+-- |         103 | Alexander   | Hunold      | AHUNOLD  | 590.423.4567       | 2006-01-03 | IT_PROG    |  9000.00 |           0.00 |        102 |            60 |
+
+-- Solutions
+
+-- Method-1
+
+    SELECT first_name,last_name, department_id
+    FROM employees
+    WHERE first_name LIKE '__s%';
+
+-- Output
+
+    --  first_name  | last_name | department_id
+    -- -------------+-----------+---------------
+    --  Jose Manuel | Urman     |           100
+    --  Jason       | Mallin    |            50
