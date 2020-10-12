7eba651b161e6c91b774532ac6d193a6f9704294
khageshwor <khageshwarjoshi03@gmail.com>
2020-10-12 22:19:37 +0545

    Add problem26 Solution

---
+++ Sorting_and_Filtering_HR/problem26.sql
@@ -0,0 +1,27 @@
+-- 26. Write a query in SQL to display the manager ID and number of employees managed by the manager.
+
+-- Sample table: employees
+
+--     +-------------+-------------+-------------+----------+--------------------+------------+------------+----------+----------------+------------+---------------+
+-- | EMPLOYEE_ID | FIRST_NAME  | LAST_NAME   | EMAIL    | PHONE_NUMBER       | HIRE_DATE  | JOB_ID     | SALARY   | COMMISSION_PCT | MANAGER_ID | DEPARTMENT_ID |
+-- +-------------+-------------+-------------+----------+--------------------+------------+------------+----------+----------------+------------+---------------+
+-- |         100 | Steven      | King        | SKING    | 515.123.4567       | 2003-06-17 | AD_PRES    | 24000.00 |           0.00 |          0 |            90 |
+-- |         101 | Neena       | Kochhar     | NKOCHHAR | 515.123.4568       | 2005-09-21 | AD_VP      | 17000.00 |           0.00 |        100 |            90 |
+-- |         102 | Lex         | De Haan     | LDEHAAN  | 515.123.4569       | 2001-01-13 | AD_VP      | 17000.00 |           0.00 |        100 |            90 |
+-- |         103 | Alexander   | Hunold      | AHUNOLD  | 590.423.4567       | 2006-01-03 | IT_PROG    |  9000.00 |           0.00 |        102 |            60 |
+
+-- Solutions
+
+-- Method-1
+
+    SELECT manager_id, COUNT(*) 
+	FROM employees 
+	GROUP BY manager_id;
+
+-- Output
+
+    --      manager_id | count
+    -- ------------+-------
+    --         205 |     1
+    --         122 |     8
+    --         120 |     8
