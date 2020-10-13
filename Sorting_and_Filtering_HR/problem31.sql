01fb01135bee9da859eff44289e9382414927d82
khageshwor <khageshwarjoshi03@gmail.com>
2020-10-13 19:00:22 +0545

    Add problem31 Solution

---
+++ Sorting_and_Filtering_HR/problem31.sql
@@ -0,0 +1,29 @@
+-- 31. Write a query in SQL to display those departments where more than ten employees 
+-- work who got a commission percentage
+
+
+-- Sample table: employees
+
+--     +-------------+-------------+-------------+----------+--------------------+------------+------------+----------+----------------+------------+---------------+
+-- | EMPLOYEE_ID | FIRST_NAME  | LAST_NAME   | EMAIL    | PHONE_NUMBER       | HIRE_DATE  | JOB_ID     | SALARY   | COMMISSION_PCT | MANAGER_ID | DEPARTMENT_ID |
+-- +-------------+-------------+-------------+----------+--------------------+------------+------------+----------+----------------+------------+---------------+
+-- |         100 | Steven      | King        | SKING    | 515.123.4567       | 2003-06-17 | AD_PRES    | 24000.00 |           0.00 |          0 |            90 |
+-- |         101 | Neena       | Kochhar     | NKOCHHAR | 515.123.4568       | 2005-09-21 | AD_VP      | 17000.00 |           0.00 |        100 |            90 |
+-- |         102 | Lex         | De Haan     | LDEHAAN  | 515.123.4569       | 2001-01-13 | AD_VP      | 17000.00 |           0.00 |        100 |            90 |
+-- |         103 | Alexander   | Hunold      | AHUNOLD  | 590.423.4567       | 2006-01-03 | IT_PROG    |  9000.00 |           0.00 |        102 |            60 |
+
+-- Solutions
+
+-- Method-1
+
+    SELECT department_id FROM Employees
+    WHERE commission_pct IS NOT NULL
+    GROUP BY department_id
+    HAVING COUNT(employee_id) > 10
+
+-- Output
+
+    --      department_id
+    -- ---------------
+    --             80
+    --             50
