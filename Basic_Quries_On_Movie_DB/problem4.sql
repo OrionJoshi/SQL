b5870022a309933514f20f53e82fbb4c833e16e6
khageshwor <khageshwarjoshi03@gmail.com>
2020-10-14 21:53:20 +0545

    Add problem4 Solution

---
+++ Basic_Quries_On_Movie_DB/problem4.sql
@@ -0,0 +1,27 @@
+-- 4. Write a query in SQL to find the movies which was released before 1998
+
+-- Sample table: movie
+
+--     mov_id |                     mov_title                      | mov_year | mov_time |    mov_lang     | mov_dt_rel | mov_rel_country
+-- --------+----------------------------------------------------+----------+----------+-----------------+------------+-----------------
+--     901 | Vertigo                                            |     1958 |      128 | English         | 1958-08-24 | UK
+--     902 | The Innocents                                      |     1961 |      100 | English         | 1962-02-19 | SW
+--     903 | Lawrence of Arabia                                 |     1962 |      216 | English         | 1962-12-11 | UK
+--     904 | The Deer Hunter                                    |     1978 |      183 | English         | 1979-03-08 | UK
+
+
+-- Solutions
+
+-- Method-1
+
+    SELECT mov_title 
+    FROM movie 
+    WHERE mov_year < '1998'
+
+-- Output
+
+    --                         mov_title
+    -- ----------------------------------------------------
+    --  Vertigo
+    --  The Innocents
+    --  Lawrence of Arabia
