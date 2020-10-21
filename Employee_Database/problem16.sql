5db0e809605de9c6a062400ca4c659334268eff1
khageshwor <khageshwarjoshi03@gmail.com>
2020-10-21 16:26:58 +0545

    Add problem16 Solution

---
+++ Employee_Database/problem16.sql
@@ -0,0 +1,25 @@
+-- 16. Write a query in SQL to list the employees whose salary is more than 3000 after giving 25% increment. 
+
+-- Sample table: employees
+
+--  emp_id | emp_name | job_name  | manager_id | hire_date  | salary  | commission | dep_id
+-- --------+----------+-----------+------------+------------+---------+------------+--------
+--   68319 | KAYLING  | PRESIDENT |            | 1991-11-18 | 6000.00 |            |   1001
+--   66928 | BLAZE    | MANAGER   |      68319 | 1991-05-01 | 2750.00 |            |   3001
+--   67832 | CLARE    | MANAGER   |      68319 | 1991-06-09 | 2550.00 |            |   1001
+--   65646 | JONAS    | MANAGER   |      68319 | 1991-04-02 | 2957.00 |            |   2001
+
+-- Solutions
+
+-- Method-1
+
+    SELECT * FROM employees 
+    WHERE (salary * 1.25) > 3000
+
+-- Output
+
+    --     emp_id | emp_name | job_name  | manager_id | hire_date  | salary  | commission | dep_id
+    -- --------+----------+-----------+------------+------------+---------+------------+--------
+    --   68319 | KAYLING  | PRESIDENT |            | 1991-11-18 | 6000.00 |            |   1001
+    --   66928 | BLAZE    | MANAGER   |      68319 | 1991-05-01 | 2750.00 |            |   3001
+
