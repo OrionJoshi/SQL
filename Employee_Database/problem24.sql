28edf963d1dfa07f752015632f38eb48f4d2a53c
khageshwor <khageshwarjoshi03@gmail.com>
2020-10-21 22:08:30 +0545

    Add problem24 Solutions

---
+++ Employee_Database/problem24.sql
@@ -0,0 +1,33 @@
+-- 24. Write a query in SQL to list the employees who have joined in the year 1991.
+
+
+-- Sample table: employees
+
+--  emp_id | emp_name | job_name  | manager_id | hire_date  | salary  | commission | dep_id
+-- --------+----------+-----------+------------+------------+---------+------------+--------
+--   68319 | KAYLING  | PRESIDENT |            | 1991-11-18 | 6000.00 |            |   1001
+--   66928 | BLAZE    | MANAGER   |      68319 | 1991-05-01 | 2750.00 |            |   3001
+--   67832 | CLARE    | MANAGER   |      68319 | 1991-06-09 | 2550.00 |            |   1001
+--   65646 | JONAS    | MANAGER   |      68319 | 1991-04-02 | 2957.00 |            |   2001
+
+-- Solutions
+
+-- -- Method-1
+
+    SELECT * FROM employees 
+    WHERE EXTRACT(YEAR FROM hire_date) = 1991
+
+-- Method-2
+
+    SELECT *
+    FROM employees
+    WHERE to_char(hire_date,'YYYY') = '1991';
+
+-- Output
+
+    --          emp_id | emp_name | job_name  | manager_id | hire_date  | salary  | commission | dep_id
+    -- --------+----------+-----------+------------+------------+---------+------------+--------
+    --   68319 | KAYLING  | PRESIDENT |            | 1991-11-18 | 6000.00 |            |   1001
+    --   66928 | BLAZE    | MANAGER   |      68319 | 1991-05-01 | 2750.00 |            |   3001
+    --   67832 | CLARE    | MANAGER   |      68319 | 1991-06-09 | 2550.00 |            |   1001
+
