3593d69cc1e958be19d068f59ebb96bb943ff1be
khageshwor <khageshwarjoshi03@gmail.com>
2020-10-22 19:24:49 +0545

    Add problem26 Solutions

---
+++ Employee_Database/problem26.sql
@@ -0,0 +1,27 @@
+-- 26. Write a query in SQL to list the employee name, and job_name who are not working under a manager.
+
+
+-- Sample table: employees
+
+--  emp_id | emp_name | job_name  | manager_id | hire_date  | salary  | commission | dep_id
+-- --------+----------+-----------+------------+------------+---------+------------+--------
+--   68319 | KAYLING  | PRESIDENT |            | 1991-11-18 | 6000.00 |            |   1001
+--   66928 | BLAZE    | MANAGER   |      68319 | 1991-05-01 | 2750.00 |            |   3001
+--   67832 | CLARE    | MANAGER   |      68319 | 1991-06-09 | 2550.00 |            |   1001
+--   65646 | JONAS    | MANAGER   |      68319 | 1991-04-02 | 2957.00 |            |   2001
+
+-- Solutions
+
+-- -- Method-1
+
+    SELECT emp_name,
+       job_name
+    FROM employees 
+    WHERE manager_id IS NULL;
+
+-- Output
+
+    --      emp_name     | job_name
+    -- --------------+--------------
+    --   KAYLING     | PRESIDENT
+
