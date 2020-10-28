37b4fad68579946f619facb0dfb357d7c4ae8bec
khageshwor <khageshwarjoshi03@gmail.com>
2020-10-28 13:40:01 +0545

    Add problem76 Solutions

---
+++ Employee_Database/problem76.sql
@@ -0,0 +1,52 @@
+-- 76. Write a query in SQL to list the unique jobs of department 2001 and 3001 in descending order.
+-- Sample table: employees
+
+--  emp_id | emp_name | job_name  | manager_id | hire_date  | salary  | commission | dep_id
+-- --------+----------+-----------+------------+------------+---------+------------+--------
+--   68319 | KAYLING  | PRESIDENT |            | 1991-11-18 | 6000.00 |            |   1001
+--   66928 | BLAZE    | MANAGER   |      68319 | 1991-05-01 | 2750.00 |            |   3001
+--   67832 | CLARE    | MANAGER   |      68319 | 1991-06-09 | 2550.00 |            |   1001
+--   65646 | JONAS    | MANAGER   |      68319 | 1991-04-02 | 2957.00 |            |   2001
+
+-- Sample table: department
+
+--     dep_id |  dep_name  | dep_location
+-- --------+------------+--------------
+--    1001 | FINANCE    | SYDNEY
+--    2001 | AUDIT      | MELBOURNE
+--    3001 | MARKETING  | PERTH
+--    4001 | PRODUCTION | BRISBANE
+
+-- Sample table: salary_grade
+
+--      grade | min_sal | max_sal
+-- -------+---------+---------
+--      1 |     800 |    1300
+--      2 |    1301 |    1500
+--      3 |    1501 |    2100
+--      4 |    2101 |    3100
+--      5 |    3101 |    9999
+-- Solutions
+
+-- Method-1
+
+    SELECT DISTINCT job_name 
+    FROM employees 
+    WHERE dep_id IN (2001, 3001) 
+    ORDER BY 1 DESC;
+
+-- Method-2
+
+    SELECT DISTINCT job_name 
+    FROM employees 
+    WHERE dep_id = 2001 OR dep_id  = 3001 
+    ORDER BY 1 DESC;
+
+-- Output
+
+    --     job_name
+    -- ----------
+    --  SALESMAN
+    --  MANAGER
+    --  CLERK
+    --  ANALYST
