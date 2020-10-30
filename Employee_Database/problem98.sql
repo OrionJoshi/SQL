4a306d3857f7f4a52ca1031c9f471f8425a687b8
khageshwor <khageshwarjoshi03@gmail.com>
2020-10-30 19:23:12 +0545

    Add problem98 Solutions

---
+++ Employee_Database/problem98.sql
@@ -0,0 +1,43 @@
+-- 98. Write a query in SQL to list the department where at least two employees are working. 
+-- Sample table: employees
+
+--  emp_id | emp_name | job_name  | manager_id | hire_date  | salary  | commission | dep_id
+-- --------+----------+-----------+------------+------------+---------+------------+--------
+--   68319 | KAYLING  | PRESIDENT |            | 1991-11-18 | 6000.00 |            |   1001
+--   66928 | BLAZE    | MANAGER   |      68319 | 1991-05-01 | 2750.00 |            |   3001
+--   67832 | CLARE    | MANAGER   |      68319 | 1991-06-09 | 2550.00 |            |   1001
+--   65646 | JONAS    | MANAGER   |      68319 | 1991-04-02 | 2957.00 |            |   2001
+
+-- Sample table: department
+
+--     dep_id |  dep_name  | dep_location
+-- --------+------------+--------------
+--    1001 | FINANCE    | SYDNEY
+--    2001 | AUDIT      | MELBOURNE
+--    3001 | MARKETING  | PERTH
+--    4001 | PRODUCTION | BRISBANE
+
+-- Sample table: salary_grade
+
+--      grade | min_sal | max_sal
+-- -------+---------+---------
+--      1 |     800 |    1300
+--      2 |    1301 |    1500
+--      3 |    1501 |    2100
+--      4 |    2101 |    3100
+--      5 |    3101 |    9999
+-- Solutions
+
+-- Method-1
+
+    SELECT dep_id, COUNT(*) 
+    FROM employees 
+    GROUP BY dep_id 
+        HAVING COUNT(*) >=2
+
+-- Method-2
+
+    SELECT dep_id, count(emp_name)
+    FROM employees
+    GROUP BY 1 HAVING count(emp_name)>=2
+    ORDER BY 2 DESC;
