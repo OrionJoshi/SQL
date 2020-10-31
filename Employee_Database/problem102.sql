412270a1a3988967f803dc4f31ccd7650b41102b
khageshwor <khageshwarjoshi03@gmail.com>
2020-10-31 19:01:12 +0545

    Add problem102 Solutions

---
+++ Employee_Database/problem102.sql
@@ -0,0 +1,62 @@
+-- 102. Write a query in SQL to list the name of departments where atleast 2 employees 
+--are working in that department.
+-- Sample table: employees
+
+--  emp_id | emp_name | job_name  | manager_id | hire_date  | salary  | commission | dep_id
+-- --------+----------+-----------+------------+------------+---------+------------+--------
+--   68319 | KAYLING  | PRESIDENT |            | 1991-11-18 | 6000.00 |            |   1001
+--   66928 | BLAZE    | MANAGER   |      68319 | 1991-05-01 | 2750.00 |            |   3001
+--   67832 | CLARE    | MANAGER   |      68319 | 1991-06-09 | 2550.00 |            |   1001
+--   65646 | JONAS    | MANAGER   |      68319 | 1991-04-02 | 2957.00 |            |   2001
+
+-- Sample table: department
+
+--     dep_id |  dep_name  | dep_location
+-- --------+------------+--------------
+--    1001 | FINANCE    | SYDNEY
+--    2001 | AUDIT      | MELBOURNE
+--    3001 | MARKETING  | PERTH
+--    4001 | PRODUCTION | BRISBANE
+
+-- Sample table: salary_grade
+
+--      grade | min_sal | max_sal
+-- -------+---------+---------
+--      1 |     800 |    1300
+--      2 |    1301 |    1500
+--      3 |    1501 |    2100
+--      4 |    2101 |    3100
+--      5 |    3101 |    9999
+-- Solutions
+
+-- Method-1
+
+    SELECT dep_name, count(emp_id) 
+    FROM employees e 
+    JOIN department d ON e.dep_id=d.dep_id 
+    GROUP BY dep_name 
+        HAVING count(emp_id)>=2;
+
+-- Method-2
+
+    SELECT d.dep_name,count(*)
+    FROM employees e,department d
+    WHERE e.dep_id = d.dep_id
+    GROUP BY d.dep_name
+    HAVING count(*) >= 2;
+
+-- Method-3
+
+    SELECT e.dep_id,d.dep_name,count(*)
+    FROM employees e, department d
+    WHERE e.dep_id=d.dep_id
+    GROUP BY 1,2 having count(*)>=2;
+
+-- Output
+
+    --     dep_name  | count
+    -- -----------+-------
+    --  FINANCE   |     3
+    --  MARKETING |     6
+    --  AUDIT     |     5
+
