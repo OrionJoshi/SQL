0a9529dc59f70aa2752db04cf88f662d79d8d0e1
khageshwor <khageshwarjoshi03@gmail.com>
2020-11-13 20:04:16 +0545

    Add problem7 Solutions

---
+++ Hospital_DB/problem7.sql
@@ -0,0 +1,28 @@
+-- 7. Write a query in SQL to count the number of unavailable rooms.
+-- Sample table: room
+
+-- roomnumber | roomtype | blockfloor | blockcode | unavailable
+-- -----------+----------+------------+-----------+-------------
+--        101 | Single   |          1 |         1 | f
+--        102 | Single   |          1 |         1 | f
+--        103 | Single   |          1 |         1 | f
+
+-- Solutions
+
+-- Method-1
+
+    SELECT count(*) AS "Number of unavailable rooms"
+    FROM room
+    WHERE unavailable='t';
+
+-- Method-2
+
+    SELECT count(*) AS "Number of unavailable rooms"
+    FROM room
+    WHERE unavailable='t%';
+
+-- Output
+
+    --     Number of available rooms
+    -- ---------------------------
+    --                          7
