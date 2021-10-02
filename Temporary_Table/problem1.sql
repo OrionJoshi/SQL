72decbb95f7f01a2d1e7b00b46ef9374f223decd
thaneshworjoshi <thaneshwor10@gmail.com>
2021-10-02 17:47:02 +0545

    Add: problem and solution of temporary table

---
+++ Temporary_Table/problem1.sql
@@ -0,0 +1,15 @@
+-- 1. Write a query to create #Employee temporary table 
+
+-- Sample table: Employee
+
+    --     employee_id |    name    |   city   | 
+    -- -------------+------------+----------+---
+    --              |              |           |       
+    --              |              |           |       
+    --              |              |           |       
+
+
+-- Solutions
+
+-- Method-1
+CREATE TABLE #Employee (employee_id INT, name VARCHAR(255), city VARCHAR(255)); 
